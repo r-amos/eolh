bc1b89afa1c67b193d3bbacc72c44d30e7407144
Robert Amos <rob@robamos.co.uk>
2019-08-24 21:20:09 +0100

    Adding Auth Scaffolding

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
-Route::prefix('routes')->name('routes.')->group(function() {
-    Route::get('/{route}', 'RoutesController@show');
-});
+Auth::routes();
 
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('routes')->name('routes.')->group(function () {
+    Route::get('/{route}', 'RoutesController@show')->name('show');
+});
