0b1385e9538606f9e3a7d96921d857895efb3060
Robert Amos <rob@robamos.co.uk>
2019-09-03 20:16:12 +0100

    Adding Email Verification Step And Restrictions

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/', 'WelcomeController@index')->name('welcome');
 
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::prefix('activities')->name('activities.')->group(function () {
-    Route::group(['middleware' => ['auth']], function () {
+    Route::group(['middleware' => ['auth', 'verified']], function () {
         Route::get('/create', 'ActivitiesController@create')->name('create');
         Route::post('/', 'ActivitiesController@store')->name('store');
     });
